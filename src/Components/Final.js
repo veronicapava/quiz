e2983739f25de5712cd49a223ba55d5e02ddfac8
Veronica Pava <veronicapava@gmail.com>
2021-09-11 17:15:02 -0500

    Se crea tabla de puntuaciones

--- src/Components/Final.js
+++ src/Components/Final.js
@@ -1,4 +1,4 @@
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 import QuizContext from "../Provider/Contexts"
 import "../index.css"
 import { connect } from "react-redux"
@@ -6,10 +6,27 @@ import { reiniciarStorage } from "../redux/actionCreators"
 
 function Final(props) {
     const { setGameState } = useContext(QuizContext)
+    const { nombreJugador, puntosCounter, rondaCounter } = props
+
+    useEffect(() => {
+        const puntuacion = {
+            nombreJugador,
+            puntos: puntosCounter,
+            ronda: rondaCounter > 5 ? "Finalista" : rondaCounter
+        }
+        const baseUrl = "http://localhost:3001/"
+        const endPoint = "puntuaciones"
+
+        fetch(baseUrl + endPoint, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(puntuacion)
+        })
+    })
     return (
         <div className="menu">
-            <h2>Jugador: {props.nombreJugador}</h2>
-            <h2>Score: ${props.puntosCounter}</h2>
+            <h2>Jugador: {nombreJugador}</h2>
+            <h2>Score: ${puntosCounter}</h2>
             <button
                 onClick={() => {
                     setGameState("menu")
--- src/Components/Tablero.js
+++ src/Components/Tablero.js
@@ -1,20 +1,42 @@
-import { useContext } from "react"
+import { useContext, useState, useEffect } from "react"
 import QuizContext from "../Provider/Contexts"
 import "../index.css"
 
 
 function Tablero() {
     const { setGameState } = useContext(QuizContext)
+    const [resultados, setResultado] = useState([])
+
+
+    useEffect(() => {
+        const baseUrl = "http://localhost:3001/"
+        const endPoint = "puntuaciones"
+        fetch(baseUrl + endPoint)
+            .then(response => response.json())
+            .then(data => setResultado(data))
+    }, [])
     return (
         <div className="tablero">
-            <h2>tablero de jugadores aca</h2>
+            <h2>Tablero de jugadores aca</h2>
+            <table>
+                <tr>
+                    <td>Jugador</td>
+                    <td>Ronda</td>
+                    <td>Score</td>
+                </tr>
+
+                {
+                    resultados.map(r =>
+                        <tr>
+                            <td>{r.nombreJugador}</td>
+                            <td>{r.puntos}</td>
+                            <td>{r.ronda}</td>
+                        </tr>
+                    )
+                }
+            </table>
 
 
-            <ul>
-                <li>nombre</li>
-                <li>ronda</li>
-                <li>puntos</li>
-            </ul>
 
             <button
                 onClick={() => {

cb931873e984a71726c8fd2fb1e91587a10f07b6
Veronica Pava <veronicapava@gmail.com>
2021-09-11 21:12:49 -0500

    Se muestra el premio a ganar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,6 @@
 import { connect } from 'react-redux';
+import { PREMIOS } from "../constantes"
+import { asignadorPremios } from "../utils"
 
 const Header = props => {
     return (
@@ -8,6 +10,7 @@ const Header = props => {
                 <h1>Ronda: {props.rondaCounter} </h1>
                 <h3>Jugador:{props.nombreJugador} </h3>
                 <h4>Score: ${props.puntosCounter}</h4>
+                <h4>En Juego: ${asignadorPremios(props.rondaCounter, PREMIOS)}</h4>
             </ul>
         </header>
     )
